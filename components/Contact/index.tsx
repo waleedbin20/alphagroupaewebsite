0572b3a7d8e93b63854ecab88bc201e65a957973
Waleed Asad <Waleed.Asad@apetito.co.uk>
2024-01-16 23:56:53 +0000

    updated contact

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -2,15 +2,84 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
+import { useState } from 'react';
+import Banner from "./banner";
 
 const Contact = () => {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState('');
   const [hasMounted, setHasMounted] = React.useState(false);
+  const [formState, setFormState] = useState({
+    fullName: '',
+    email: '',
+    subject: '',
+    contactPhoneNumber: '',
+    message: '',
+  });
+
   React.useEffect(() => {
     setHasMounted(true);
   }, []);
+
   if (!hasMounted) {
     return null;
   }
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (
+      !formState.fullName ||
+      !formState.email ||
+      !formState.subject ||
+      !formState.contactPhoneNumber ||
+      !formState.message
+    ) {
+      setError('Please fill in all fields.');
+      setTimeout(() => {
+        setError('');
+      }, 3000);
+
+
+      return;
+    }
+    try {
+      const formData = new FormData();
+
+      Object.entries(formState).forEach(([key, value]) => {
+        formData.append(key, value);
+      });
+      const response = await fetch('https://alphagroup-api.azurewebsites.net/Customer', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify(formState),
+      });
+
+      if (response.ok) {
+        setSuccess(true);
+
+        setFormState({
+          fullName: '',
+          email: '',
+          subject: '',
+          contactPhoneNumber: '',
+          message: '',
+        });
+        setTimeout(() => {
+          setSuccess(false);
+        }, 6000);
+
+      } else {
+        console.error('Failed to send form data to the backend');
+      }
+    } catch (error) {
+      console.error('Error handling form submission:', error);
+    }
+  };
 
   return (
     <>
@@ -57,19 +126,23 @@ const Contact = () => {
               </h2>
 
               <form
-                action="https://formbold.com/s/unique_form_id"
+                onSubmit={handleSubmit}
                 method="POST"
               >
                 <div className="mb-7.5 flex flex-col gap-7.5 lg:flex-row lg:justify-between lg:gap-14">
                   <input
                     type="text"
                     placeholder="Full name"
+                    value={formState.fullName}
+                    onChange={(e) => setFormState({ ...formState, fullName: e.target.value })}
                     className="w-full border-b border-stroke bg-transparent pb-3.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white lg:w-1/2"
                   />
 
                   <input
                     type="email"
                     placeholder="Email address"
+                    value={formState.email}
+                    onChange={(e) => setFormState({ ...formState, email: e.target.value })}
                     className="w-full border-b border-stroke bg-transparent pb-3.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white lg:w-1/2"
                   />
                 </div>
@@ -78,12 +151,16 @@ const Contact = () => {
                   <input
                     type="text"
                     placeholder="Subject"
+                    value={formState.subject}
+                    onChange={(e) => setFormState({ ...formState, subject: e.target.value })}
                     className="w-full border-b border-stroke bg-transparent pb-3.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white lg:w-1/2"
                   />
 
                   <input
                     type="text"
                     placeholder="Phone number"
+                    value={formState.contactPhoneNumber}
+                    onChange={(e) => setFormState({ ...formState, contactPhoneNumber: e.target.value })}
                     className="w-full border-b border-stroke bg-transparent pb-3.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white lg:w-1/2"
                   />
                 </div>
@@ -91,6 +168,8 @@ const Contact = () => {
                 <div className="mb-11.5 flex">
                   <textarea
                     placeholder="Message"
+                    value={formState.message}
+                    onChange={(e) => setFormState({ ...formState, message: e.target.value })}
                     rows={4}
                     className="w-full border-b border-stroke bg-transparent focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white"
                   ></textarea>
@@ -103,34 +182,11 @@ const Contact = () => {
                       type="checkbox"
                       className="peer sr-only"
                     />
-                    <span className="border-gray-300 bg-gray-100 text-blue-600 dark:border-gray-600 dark:bg-gray-700 group mt-2 flex h-5 min-w-[20px] items-center justify-center rounded peer-checked:bg-primary">
-                      <svg
-                        className="opacity-0 peer-checked:group-[]:opacity-100"
-                        width="10"
-                        height="8"
-                        viewBox="0 0 10 8"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M9.70704 0.792787C9.89451 0.980314 9.99983 1.23462 9.99983 1.49979C9.99983 1.76495 9.89451 2.01926 9.70704 2.20679L4.70704 7.20679C4.51951 7.39426 4.26521 7.49957 4.00004 7.49957C3.73488 7.49957 3.48057 7.39426 3.29304 7.20679L0.293041 4.20679C0.110883 4.01818 0.0100885 3.76558 0.0123669 3.50339C0.0146453 3.24119 0.119814 2.99038 0.305222 2.80497C0.490631 2.61956 0.741443 2.51439 1.00364 2.51211C1.26584 2.50983 1.51844 2.61063 1.70704 2.79279L4.00004 5.08579L8.29304 0.792787C8.48057 0.605316 8.73488 0.5 9.00004 0.5C9.26521 0.5 9.51951 0.605316 9.70704 0.792787Z"
-                          fill="white"
-                        />
-                      </svg>
-                    </span>
-                    <label
-                      htmlFor="default-checkbox"
-                      className="flex max-w-[425px] cursor-pointer select-none pl-5"
-                    >
-                      By clicking Checkbox, you agree to use our “Form” terms
-                      And consent cookie usage in browser.
-                    </label>
                   </div>
 
                   <button
                     aria-label="send message"
+                    type="submit"
                     className="inline-flex items-center gap-2.5 rounded-full bg-black px-6 py-3 font-medium text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark"
                   >
                     Send Message
@@ -150,7 +206,11 @@ const Contact = () => {
                   </button>
                 </div>
               </form>
+              <br />
+              {error && <Banner message={error} message1="" />}
+              {success && <Banner message="Your Message Has Been Sent Successfully" message1=" Our team will get back to your shortly." />}
             </motion.div>
+
 
             <motion.div
               variants={{
@@ -199,7 +259,7 @@ const Contact = () => {
             </motion.div>
           </div>
         </div>
-      </section>
+      </section >
       {/* <!-- ===== Contact End ===== --> */}
     </>
   );
